9b4c56dab359b0859459b6194dd56c38240993ce
jiajiamayue <132677405+jiajiamayue@users.noreply.github.com>
2024-04-07 18:02:57 +0800

    Update dag2file.go

--- dag2file.go
+++ dag2file.go
@@ -1,82 +1,61 @@
 package merkledag
 
 import (
-	"encoding/json"
+	"bytes"
+	"encoding/gob"
 	"strings"
 )
 
-const STEP = 4
-
-// Hash to file
-
-// example a path : /doc/tmp/temp.txt
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
-	// 根据hash和path， 返curObjBinary回对应的文件, hash对应的类型是tree
-	flag, _ := store.Has(hash)
-	if flag {
-		objBinary, _ := store.Get(hash)
-		obj := binaryToObj(objBinary)
-		pathArr := strings.Split(path, "/")
-		cur := 1
-		return getFileByDir(obj, pathArr, cur, store)
-	}
-	return nil
-}
+	rootObjBytes, _ := store.Get(hash)
+	rootObj, _ := deserialize(rootObjBytes)
+	currentNode := rootObj
+	pathComponents := splitPath(path)
 
-func getFileByDir(obj *Object, pathArr []string, cur int, store KVStore) []byte {
-	if cur >= len(pathArr) {
+	for _, component := range pathComponents {
+		if currentNode.Data[0] != "tree" {
+			return nil
+		}
+
+		childHash := findChildHash(currentNode, component)
+		if childHash == nil {
+			return nil
+		}
+		childObjBytes, _ := store.Get(childHash)
+		childObj, _ := deserialize(childObjBytes)
+
+		currentNode = childObj
+	}
+
+	if currentNode.Data[0] != "blob" {
 		return nil
 	}
-	index := 0
-	for i := range obj.Links {
-		objType := string(obj.Data[index : index+STEP])
-		index += STEP
-		objInfo := obj.Links[i]
-		if objInfo.Name != pathArr[cur] {
-			continue
-		}
-		switch objType {
-		case TREE:
-			objDirBinary, _ := store.Get(objInfo.Hash)
-			objDir := binaryToObj(objDirBinary)
-			ans := getFileByDir(objDir, pathArr, cur+1, store)
-			if ans != nil {
-				return ans
-			}
-		case BLOB:
-			ans, _ := store.Get(objInfo.Hash)
-			return ans
-		case LIST:
-			objLinkBinary, _ := store.Get(objInfo.Hash)
-			objList := binaryToObj(objLinkBinary)
-			ans := getFileByList(objList, store)
-			return ans
+
+	fileBytes, _ := store.Get([]byte(currentNode.Links[0].Hash))
+	return fileBytes
+}
+
+// 拆分路径
+func splitPath(path string) []string {
+	return strings.Split(path, "/")
+}
+
+// 反序列化object
+func deserialize(data []byte) (*Object, error) {
+	var obj Object
+	err := gob.NewDecoder(bytes.NewReader(data)).Decode(&obj)
+	if err != nil {
+		return nil, err
+	}
+	return &obj, nil
+}
+
+// 根据子节点名称查找对应的哈希值
+func findChildHash(node *Object, name string) []byte {
+	for _, link := range node.Links {
+		if link.Name == name {
+			return []byte(link.Hash)
 		}
 	}
 	return nil
-}
-
-func getFileByList(obj *Object, store KVStore) []byte {
-	ans := make([]byte, 0)
-	index := 0
-	for i := range obj.Links {
-		curObjType := string(obj.Data[index : index+STEP])
-		index += STEP
-		curObjLink := obj.Links[i]
-		curObjBinary, _ := store.Get(curObjLink.Hash)
-		curObj := binaryToObj(curObjBinary)
-		if curObjType == BLOB {
-			ans = append(ans, curObjBinary...)
-		} else { //List
-			tmp := getFileByList(curObj, store)
-			ans = append(ans, tmp...)
-		}
-	}
-	return ans
-}
-
-func binaryToObj(objBinary []byte) *Object {
-	var res Object
-	json.Unmarshal(objBinary, &res)
-	return &res
 }
